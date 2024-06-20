f65ab9b3158f277d5ee4f7812988ead18e1341a4
Fatoumata Badji <60388963+Fatoumata964@users.noreply.github.com>
2024-06-20 10:34:25 +0200

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -3,18 +3,14 @@ import locale
 locale.getpreferredencoding = lambda: "UTF-8"
 
 # Importation de la classe HuggingFaceHub depuis le module langchain
-from llama_index.llms.huggingface import HuggingFaceLLM
+from langchain import HuggingFaceHub
 
 # Définition de la fonction llm qui crée et retourne un modèle de langage mixte
-def llm(system_prompt):
+def llm():
     # Création d'une instance de la classe HuggingFaceHub avec les paramètres spécifiés
-    llm_model = HuggingFaceLLM(
-            context_window=4096,
-            max_new_tokens=256,
-            model_name="TheBloke/Mixtral-8x7B-v0.1-GPTQ",
-            tokenizer_name="TheBloke/Mixtral-8x7B-v0.1-GPTQ",  # Identifiant du dépôt du modèle
-            generate_kwargs={"temperature": 0.0, "do_sample": False},
-            system_prompt=system_prompt,
+    llm_model = HuggingFaceHub(
+        repo_id='mistralai/Mixtral-8x7B-Instruct-v0.1',  # Identifiant du dépôt du modèle
+        model_kwargs={'max_length': 700, 'temperature': 0.1, 'return_full_text':False}  # Arguments du modèle (longueur maximale et température)
     )
     # Retour de l'instance du modèle de langage mixte créée
     return llm_model
