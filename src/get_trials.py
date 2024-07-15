ef4d8c4f5d6a9abf67c9ed9ada41622b7fd47367
Fatoumata Badji <60388963+Fatoumata964@users.noreply.github.com>
2024-07-15 14:24:23 +0200

    Update get_trials.py

--- src/get_trials.py
+++ src/get_trials.py
@@ -173,8 +173,13 @@ def extract_regulation(drug, countries, eudract, disease):
       
       # Joindre les réponses
         response = '\n\n'.join(responses1)
-        
+
+        for el in [sim['Substance active'].iloc[0], sim['Nom du médicament'].iloc[0]]:
+              if el in response:
+                  response = response.replace(el, drug)
+                    
         responses = f"CECI EST UN EXEMPLE D'ESSAI CLINIQUE PROCHE DE CELUI DEMANDE.\n\n{response}"
+        
 
     return responses
     
